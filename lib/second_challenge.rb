ed87d05caaadfb4a90c037ce354d26d960adc57d
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-07 11:42:56 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -13,7 +13,10 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => []}
+      :heroine_friends => []
+      
+    }
+  }
 
   
 end
