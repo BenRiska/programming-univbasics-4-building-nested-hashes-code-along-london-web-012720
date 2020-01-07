3892c334e20540707a1eadbf9664aeee76a9a3c0
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-07 11:38:59 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -12,6 +12,13 @@ def second_challenge
       }, 
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Nurse", age: "44", attitude: "worried"}
+      ]
       }}
 
   
